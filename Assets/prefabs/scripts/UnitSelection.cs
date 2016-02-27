6efdc2f06807b8474bcfe4eb5748c88cce2ea2f8
ladam92 <caesar456@gmail.com>
2016-02-27 23:21:41 +0100

    added dragbox

--- Assets/prefabs/scripts/UnitSelection.cs
+++ Assets/prefabs/scripts/UnitSelection.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class UnitSelection : MonoBehaviour {
 
     private RaycastHit hit;
-    public static GameObject CurrentlySelectedUnit;
-    private Vector3 mouseDownPoint;
+    private Vector3 mouseDownPointWorldSpace;
+    private Vector3 mouseUpPointWorldSpace;
+    private Vector3 mouseCurentPointWorldSpace;
+    private bool userIsDraggingMouse;
+    private float timeLeftBeforeDeclareMouseDrag;
+    private Vector2 mouseDragStartInScreenSpace;
+
+    public static List<GameObject> CurrentlySelectedUnits = new List<GameObject>();
+    
+    public float ClickOffset = 5.0f;
+    public float DragOffset = 5.0f;
+    public float TimeLimitBeforeDeclareMouseDrag = 1.0f;
+    public GUIStyle MouseDragSkin;
 
 	// Use this for initialization
 	void Start ()
     {
-        mouseDownPoint = Vector3.zero;
-        Debug.Log("Mouse selection script started");
+        mouseDownPointWorldSpace = Vector3.zero;
 	}
 	
 	// Update is called once per frame
@@ -21,58 +31,132 @@ public class UnitSelection : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            Debug.Log(hit.collider.name);
-            //left mouse button
-            if (Input.GetMouseButtonDown(0))
-            {
-                mouseDownPoint = hit.point;
-            }
+            Debug.DrawRay(ray.origin, ray.direction * 1000.0f, Color.yellow);
+            mouseCurentPointWorldSpace = hit.point;
 
-            if (hit.collider.name == "Terrain")
+            if (Input.GetMouseButtonDown(0)) //controlKey
             {
-                if (Input.GetMouseButtonUp(0) && DidUserClickleftMouse(hit.point))
+                mouseDownPointWorldSpace = hit.point;
+                timeLeftBeforeDeclareMouseDrag = TimeLimitBeforeDeclareMouseDrag;
+                mouseDragStartInScreenSpace = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0)) //controlKey
+            {
+                if(!userIsDraggingMouse)
                 {
-                    DeselectGameobjectIfSelected();
+                    timeLeftBeforeDeclareMouseDrag -= Time.deltaTime;
+
+                    if(timeLeftBeforeDeclareMouseDrag <= 0 || IsUserDraggingMouse(mouseDragStartInScreenSpace, Input.mousePosition))
+                    {
+                        userIsDraggingMouse = true;
+                    }
+                }
+
+                if(userIsDraggingMouse)
+                {
+                    Debug.Log("User is dragging");
                 }
             }
-            else
+            else if (Input.GetMouseButtonUp(0)) //controlKey
             {
-                if (Input.GetMouseButtonUp(0) && DidUserClickleftMouse(hit.point))
+                timeLeftBeforeDeclareMouseDrag = 0;
+                userIsDraggingMouse = false;
+            }
+
+            if (!userIsDraggingMouse)
+            {
+                if (hit.collider.name == "Terrain")
                 {
-                    if(hit.collider.transform.parent != null && hit.collider.transform.parent.FindChild("SelectionVisualizer"))
+                    if (Input.GetMouseButtonUp(0) && DidUserClickleftMouse(hit.point)) //controlKey
                     {
-                        Debug.Log("User selected a Unit!");
-
-                        if (CurrentlySelectedUnit != hit.collider.transform.parent.gameObject)
+                        DeselectUnitsIfNoShiftKeysPressed();
+                    }
+                }
+                else
+                {
+                    if (Input.GetMouseButtonUp(0) && DidUserClickleftMouse(hit.point)) //controlKey
+                    {
+                        if (hit.collider.transform.parent != null && hit.collider.transform.parent.FindChild("SelectionVisualizer"))
                         {
-                            DeselectGameobjectIfSelected();
-                            CurrentlySelectedUnit = hit.collider.transform.parent.gameObject;
+                            GameObject parentObject = hit.collider.transform.parent.gameObject;
 
-                            GameObject selectedObject = hit.collider.transform.parent.FindChild("SelectionVisualizer").gameObject;
-                            selectedObject.SetActive(true);
+                            if (!CurrentlySelectedUnits.Contains(parentObject))
+                            {
+                                bool isShiftKeysDown = IsShiftKeysDown();
+
+                                if (isShiftKeysDown)
+                                {
+                                    AddNewUnitToCurrentlySelectedUnits(parentObject);
+                                }
+                                else
+                                {
+                                    DeselectUnitsIfSelected();
+                                    AddNewUnitToCurrentlySelectedUnits(parentObject);
+                                }
+                            }
+                            else
+                            {
+                                bool isShiftKeysDown = IsShiftKeysDown();
+
+                                if (isShiftKeysDown)
+                                {
+                                    RemoveUnitFromCurentlySelectedUnits(parentObject);
+                                }
+                                else
+                                {
+                                    DeselectUnitsIfSelected();
+                                    AddNewUnitToCurrentlySelectedUnits(parentObject);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            DeselectUnitsIfNoShiftKeysPressed();
                         }
                     }
                 }
             }
-        }
-        else
-        {
-            if(Input.GetMouseButtonUp(0) && DidUserClickleftMouse(hit.point))
+            else
             {
-                DeselectGameobjectIfSelected();
+                if (Input.GetMouseButtonUp(0) && DidUserClickleftMouse(hit.point)) //controlKey
+                {
+                    DeselectUnitsIfNoShiftKeysPressed();
+                }
             }
         }
 	}
+
+    void OnGUI()
+    {
+
+        if (userIsDraggingMouse)
+        {
+            var boxWidth = Camera.main.WorldToScreenPoint(mouseDownPointWorldSpace).x - Camera.main.WorldToScreenPoint(mouseCurentPointWorldSpace).x;
+            var boxHeight = Camera.main.WorldToScreenPoint(mouseDownPointWorldSpace).y - Camera.main.WorldToScreenPoint(mouseCurentPointWorldSpace).y;
+            var boxLeft = Input.mousePosition.x;
+            var boxTop = (Screen.height - Input.mousePosition.y) - boxHeight;
+
+            GUI.Box(new Rect(boxLeft, boxTop, boxWidth, boxHeight), "", MouseDragSkin);
+        }
+    }
+
+    private void DeselectUnitsIfNoShiftKeysPressed()
+    {
+        bool isShiftKeysDown = IsShiftKeysDown();
+
+        if (!isShiftKeysDown)
+        {
+            DeselectUnitsIfSelected();
+        }
+    }
 
     private bool DidUserClickleftMouse(Vector3 leftMousePoint)
     {
         bool ret = false;
 
-        float clickoffset = 10f;
-
-        if((mouseDownPoint.x < leftMousePoint.x + clickoffset && mouseDownPoint.x > leftMousePoint.x - clickoffset) &&
-            (mouseDownPoint.y < leftMousePoint.y + clickoffset && mouseDownPoint.y > leftMousePoint.y - clickoffset) &&
-            (mouseDownPoint.z < leftMousePoint.z + clickoffset && mouseDownPoint.z > leftMousePoint.z - clickoffset))
+        if((mouseDownPointWorldSpace.x < leftMousePoint.x + ClickOffset && mouseDownPointWorldSpace.x > leftMousePoint.x - ClickOffset) &&
+            (mouseDownPointWorldSpace.y < leftMousePoint.y + ClickOffset && mouseDownPointWorldSpace.y > leftMousePoint.y - ClickOffset) &&
+            (mouseDownPointWorldSpace.z < leftMousePoint.z + ClickOffset && mouseDownPointWorldSpace.z > leftMousePoint.z - ClickOffset))
         {
             ret = true;
         }
@@ -80,12 +164,49 @@ public class UnitSelection : MonoBehaviour {
         return ret;
     }
 
-    private void DeselectGameobjectIfSelected()
+    private void DeselectUnitsIfSelected()
     {
-        if (CurrentlySelectedUnit != null)
+        if (CurrentlySelectedUnits.Count != 0)
         {
-            CurrentlySelectedUnit.transform.FindChild("SelectionVisualizer").gameObject.SetActive(false);
-            CurrentlySelectedUnit = null;
+            CurrentlySelectedUnits.ForEach(i => SetSelectionVisualizer(i, false));
+            CurrentlySelectedUnits.Clear();
         }
+    }
+
+    private void RemoveUnitFromCurentlySelectedUnits(GameObject unit)
+    {
+        if(CurrentlySelectedUnits.Remove(unit))
+        {
+            SetSelectionVisualizer(unit, false);
+        }
+    }
+
+    private void SetSelectionVisualizer(GameObject unit, bool active)
+    {
+        unit.transform.FindChild("SelectionVisualizer").gameObject.SetActive(active);
+    }
+
+    private bool IsShiftKeysDown()
+    {
+        return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift); //controlKey
+    }
+
+    private void AddNewUnitToCurrentlySelectedUnits(GameObject unit)
+    {
+        CurrentlySelectedUnits.Add(unit);
+        SetSelectionVisualizer(unit, true);
+    }
+
+    private bool IsUserDraggingMouse(Vector2 DragStart, Vector2 NewPoint)
+    {
+        bool ret = false;
+
+        if((NewPoint.x > DragStart.x + DragOffset || NewPoint.x < DragStart.x - DragOffset) &&
+            (NewPoint.y > DragStart.y + DragOffset || NewPoint.y < DragStart.y - DragOffset))
+        {
+            ret = true;
+        }
+
+        return ret;
     }
 }
