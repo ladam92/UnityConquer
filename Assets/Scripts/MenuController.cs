810c939fbbc73041f785251a9f3ab2280bc529a8
ladam92 <caesar456@gmail.com>
2016-04-17 18:28:20 +0200

    added menu, deleted Library folder, added gitginore

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGameButtonClicked()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
